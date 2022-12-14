a0cfff2dc7fe58757453588a4c498f8f26f7538b
Van Thuan Nguyen <vanthuan2004@gmail.com>
2022-12-14 20:51:02 +0700

    Small update
    Image

--- target/generated-sources/annotations/com/nhom8/entity/CartItem_.java
+++ target/generated-sources/annotations/com/nhom8/entity/CartItem_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2022-12-14T20:16:05")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2022-12-14T20:47:32")
 @StaticMetamodel(CartItem.class)
 public class CartItem_ { 
 
